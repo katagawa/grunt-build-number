5110bb2642b4fafdf3df16f75f051954417601d4
DhC <katagawa@gmail.com>
2018-12-02 21:06:45 -0500

    update dependencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,27 +6,29 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
+"use strict";
 
 module.exports = function (grunt) {
   // load all npm grunt tasks
-  require('load-grunt-tasks')(grunt);
+  require("load-grunt-tasks")(grunt);
 
   // Project configuration.
   grunt.initConfig({
     jshint: {
       all: [
-        'Gruntfile.js',
-        'tasks/*.js'
+        "Gruntfile.js",
+        "package.json",
+        "package-lock.json",
+        "tasks/*.js"
       ],
       options: {
-        jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish')
+        jshintrc: ".jshintrc",
+        reporter: require("jshint-stylish")
       }
     }
   });
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask("default", ["jshint"]);
 
 };
